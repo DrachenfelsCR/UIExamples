b8df6ac57417b634e6671c13456961c1b28a54bf
DrachenfelsCR <davidquesadaromero@gmail.com>
2022-05-16 03:18:30 -0600

    First push

---
+++ app/src/main/java/com/example/uiexamples/EditPersonActivity.kt
@@ -0,0 +1,12 @@
+package com.example.uiexamples
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditPersonActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_person)
+
+    }
+}
